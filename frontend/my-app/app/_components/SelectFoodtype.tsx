82653463cac836965653010b62cce8ee10ca2c24
zawataku <c1254976@st.kanazawa-it.ac.jp>
2024-07-29 02:36:02 +0900

    フ�ントの実装

--- frontend/my-app/app/_components/SelectFoodtype.tsx
+++ frontend/my-app/app/_components/SelectFoodtype.tsx
@@ -10,7 +10,7 @@ interface SelectButtonProps {
 const SelectFoodtype: React.FC<SelectButtonProps> = ({ onSelect }) => {
     return (
         <div className="w-full">
-            <h1 className='text-2xl font-bold py-5'>好みの料理タイプを選んでね！</h1>
+            <h1 className='text-2xl font-bold py-5'>今日はどんな料理の気分...？</h1>
             <div className="mx-auto flex justify-between pt-3">
                 <input type="radio" name="type" className="btn btn-wide text-lg" aria-label="和食"
                     onClick={() => onSelect('japaneseCuisineValue')}
