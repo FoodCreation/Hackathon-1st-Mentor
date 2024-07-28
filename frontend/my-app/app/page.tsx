19393d2765d8a6d1776c688978f9dbb50ef7ab0e
zawataku <c1254976@st.kanazawa-it.ac.jp>
2024-07-28 16:22:09 +0900

    Create Footer

--- frontend/my-app/app/page.tsx
+++ frontend/my-app/app/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./_components/Header/Header";
+import Footer from "./_components/Footer/Footer";
 
 export default function Home() {
   return (
     <div className="lg:max-w-full">
       <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
