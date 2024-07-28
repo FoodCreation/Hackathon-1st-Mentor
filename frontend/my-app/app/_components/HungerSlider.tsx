941d9bdc0827c92f066c07c44cac514040f578f9
zawataku <c1254976@st.kanazawa-it.ac.jp>
2024-07-28 17:10:23 +0900

    Fix Hunger Slider

--- frontend/my-app/app/_components/HungerSlider.tsx
+++ frontend/my-app/app/_components/HungerSlider.tsx
@@ -1,17 +1,37 @@
-import React from 'react';
-import { useEffect } from 'react';
+"use client"
+import React, { useState } from 'react';
 
-export default function HungerSlider () {
+export default function HungerSlider() {
+    const [hungerLevel, setHungerLevel] = useState(6);
+
+    const handleSliderChange = (event) => {
+        setHungerLevel(event.target.value);
+    };
+
     return (
-        <div>
-            <input type="range" min="0" max="100" value="25" className="range" step="25" />
-            <div className="flex w-full justify-between px-2 text-xs">
-                <span>|</span>
-                <span>|</span>
-                <span>|</span>
-                <span>|</span>
-                <span>|</span>
+        <div className='my-10'>
+            <input
+                type="range"
+                min={1}
+                max="10"
+                value={hungerLevel}
+                onChange={handleSliderChange}
+                step="1"
+                className="range range-lg"
+            />
+            <div className="flex w-full justify-between text-base">
+                <span className='pl-2'>1</span>
+                <span>2</span>
+                <span>3</span>
+                <span>4</span>
+                <span>5</span>
+                <span>6</span>
+                <span>7</span>
+                <span>8</span>
+                <span>9</span>
+                <span className='pr-2'>10</span>
             </div>
+            <div>Hunger Level: {hungerLevel}</div>
         </div>
-    )
+    );
 }
--- frontend/my-app/app/page.tsx
+++ frontend/my-app/app/page.tsx
@@ -4,13 +4,12 @@ import HungerSlider from "./_components/HungerSlider";
 
 export default function Home() {
   return (
-    <>
-      <div className="lg:max-w-full">
-        <Header></Header>
-      </div>
-      <div>
+    <div className="lg:max-w-full">
+      <Header></Header>
+      <div className="max-w-4xl mx-auto">
         <HungerSlider></HungerSlider>
       </div>
-    </>
+    </div>
+
   );
 }
