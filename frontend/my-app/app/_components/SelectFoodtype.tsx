6ff2a3f2a27e960644d7b95f121258a08b0395d5
zawataku <c1254976@st.kanazawa-it.ac.jp>
2024-07-29 23:41:45 +0900

    SelectFoodtypeのレスポンシブ対応

--- frontend/my-app/app/_components/SelectFoodtype.tsx
+++ frontend/my-app/app/_components/SelectFoodtype.tsx
@@ -6,12 +6,28 @@ type ButtonValue = 'chineseCuisineValue' | 'japaneseCuisineValue' | 'westernCuis
 interface SelectButtonProps {
     onSelect: (value: ButtonValue) => void;
 }
-
 const SelectFoodtype: React.FC<SelectButtonProps> = ({ onSelect }) => {
+
+    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const value = event.target.value;
+        switch (value) {
+            case '和食':
+                onSelect('japaneseCuisineValue');
+                break;
+            case '洋食':
+                onSelect('westernCuisineValue');
+                break;
+            case '中華':
+                onSelect('chineseCuisineValue');
+                break;
+            default:
+                break;
+        }
+    };
     return (
         <div className="w-full my-10">
-            <h1 className='text-2xl font-bold pb-5'>今日はどんな料理の気分...？</h1>
-            <div className="mx-auto flex justify-between pt-3">
+            <h1 className='text-2xl font-bold pb-5 text-center md:text-left'>今日はどんな料理の気分...？</h1>
+            <div className="hidden md:mx-auto md:flex md:justify-between md:pt-3">
                 <input type="radio" name="type" className="btn btn-wide text-lg" aria-label="和食"
                     onClick={() => onSelect('japaneseCuisineValue')}
                 ></input>
@@ -21,6 +37,15 @@ const SelectFoodtype: React.FC<SelectButtonProps> = ({ onSelect }) => {
                 <input type="radio" name="type" className="btn btn-wide text-lg" aria-label="中華"
                     onClick={() => onSelect('chineseCuisineValue')}
                 ></input>
+            </div>
+
+            <div className="mx-auto md:hidden">
+                <select className="select select-primary select-bordered w-full text-lg" onChange={handleChange}>
+                    <option disabled selected>選択してね</option>
+                    <option>和食</option>
+                    <option>洋食</option>
+                    <option>中華</option>
+                </select>
             </div>
         </div>
     );
